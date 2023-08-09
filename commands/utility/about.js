78b627bbd7cb5f45b60298ca227ccd42961f16d3
Andreas Miller <73205713+A9qx@users.noreply.github.com>
2023-08-09 09:53:11 -0600

    accidentally deleted footer, replaced

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -12,6 +12,7 @@ module.exports = {
             .setURL('https://github.com/A9qx/Christian')
             .setImage('https://static.wikia.nocookie.net/villainsfanon/images/4/4e/Troll-Face-Meme-PNG.png/revision/latest/scale-to-width-down/1200?cb=20190104124219')
             .setTimestamp()
+            .setFooter({ text: 'Created by A9qx', iconURL: 'https://cdn.discordapp.com/avatars/563087321704955914/76bae140071868346e001db57fd3c16a.webp' });
 
         await interaction.reply({ embeds: [aboutEmbed] })
     },
