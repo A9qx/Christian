62371da14ce6cc58829ee4f1a0692e700fce231b
Andreas Miller <73205713+A9qx@users.noreply.github.com>
2023-08-09 10:52:40 -0600

    added command list

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -11,6 +11,16 @@ module.exports = {
             .setDescription("This project is **purely a joke**, no harm intended. Based on my IRL friend Christian. Code is open-source on GitHub (linked).")
             .setURL('https://github.com/A9qx/Christian')
             .setImage('https://static.wikia.nocookie.net/villainsfanon/images/4/4e/Troll-Face-Meme-PNG.png/revision/latest/scale-to-width-down/1200?cb=20190104124219')
+            .addFields(
+                { name: '/about', value: 'Get information about the bot.', inline: true },
+                { name: '/ping', value: 'Tests the latency between you and Discord!', inline: true },
+                { name: '/version', value: 'View information regarding this build.', inline: true },
+                { name: '/anime', value: 'Find out your inner Anime!', inline: true },
+                { name: '/gay', value: 'Rate a user\'s gayness!', inline: true },
+                { name: '/joke', value: 'Get a random joke', inline: true },
+                { name: '/quote', value: "I will give you something I've actually before.", inline: true },
+                { name: '/rps', value: 'Play rock-paper-scissors with Me!', inline: true },
+            )
             .setTimestamp()
             .setFooter({ text: 'Created by A9qx', iconURL: 'https://cdn.discordapp.com/avatars/563087321704955914/76bae140071868346e001db57fd3c16a.webp' });
 
