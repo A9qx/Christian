91c527664461b3381da24b17c20435454eb8cb0c
Andreas Miller <73205713+A9qx@users.noreply.github.com>
2023-08-08 23:29:32 -0600

    added status to say "Listening to Spotify"

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,14 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType, Status } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		client.user.setActivity({
+			name: "Spotify",
+			type: ActivityType.Listening,
+		});
 	},
 };
