011488fa8c2059dbfc0ef6ccd2898a6655ba340b
Andreas Miller <73205713+A9qx@users.noreply.github.com>
2023-08-09 09:52:59 -0600

    added more quotes

--- commands/fun/quote.js
+++ commands/fun/quote.js
@@ -17,7 +17,10 @@ module.exports = {
             "we should make a new doorsmp but with the vivecraft mod",
             "well technically you can @ everyone since you're mod",
             "i think i can get my parents to go half and half on this and then boom",
-            "it's ok, we can buy a new one"
+            "it's ok, we can buy a new one",
+            "I am only available from 12:00-18:00 MST on Monday-Friday. (this may change if I am not busy on the weekends, but this is the general idea",
+            "Hi there! I'm the owner of Azure Printing, falsonix. I started this business as a way for people to get affordable 3D prints with good quality. If you have any questions or want to start an order, go to ⁠get-a-quote and click the button there. Thanks!",
+            "***✅ TacticalSoupCan#9239 has been warned. || Don't abuse admin, please.***"
         ];
 
         const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
