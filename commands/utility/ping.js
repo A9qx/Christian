4f2004adfef114809e1dde6ba59ffbb5dcfff232
Andreas Miller <73205713+A9qx@users.noreply.github.com>
2023-08-09 10:43:28 -0600

    converted to embeds to look better

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,20 +1,39 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Tests the latency between you and Discord!'),
 	async execute(interaction) {
+        const pingingEmbed = new EmbedBuilder()
+        .setTitle("Ping!?!?...")
+        .setColor("#ffffff")
+
+        const sentTimestamp = Date.now();
+        const reply = await interaction.reply({ embeds: [pingingEmbed] });
+
+        const latency = Math.abs(reply.createdTimestamp - sentTimestamp);
+
 		const shouldMiss = Math.random() < 0.5;
 
 		if (shouldMiss) {
-			return interaction.reply("Whoops, I missed, try again");
-		} else {
-			const sentTimestamp = Date.now();
-			const reply = await interaction.reply('Pinging...');
+            const missEmbed = new EmbedBuilder()
+            .setTitle("Whoops!")
+            .setColor("#fc0324")
+            .setDescription("I missed the ball, try again, i forgor how to play ping pong")
+            .setImage('https://thumbs.gfycat.com/RashShrillBluegill-size_restricted.gif')
+            .setTimestamp()
 
-			const latency = Math.abs(reply.createdTimestamp - sentTimestamp);
-			return reply.edit(`Pong! (${latency}ms)`);
+            return reply.edit({ embeds: [missEmbed] });
+		} else {
+            const madeitEmbed = new EmbedBuilder()
+            .setTitle(`Pong! (${latency}ms)`)
+            .setColor("#39e31b")
+            .setDescription("Wow! I made it! Throw me another one!")
+            .setImage('https://i.gifer.com/3eAS.gif')
+            .setTimestamp()
+
+            return reply.edit({ embeds: [madeitEmbed] });
 		}
 	},
 };
