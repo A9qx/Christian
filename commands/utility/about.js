dfd5fa0b91553584d5d4005fa757095ee85644b6
Andreas Miller <73205713+A9qx@users.noreply.github.com>
2023-08-09 09:19:07 -0600

    changed from thumbnail to image

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -10,7 +10,7 @@ module.exports = {
             .setColor("#ffffff")
             .setDescription("This project is **purely a joke**, no harm intended. Based on my IRL friend Christian. Code is open-source on GitHub (linked).")
             .setURL('https://github.com/A9qx/Christian')
-            .setThumbnail('https://static.wikia.nocookie.net/villainsfanon/images/4/4e/Troll-Face-Meme-PNG.png/revision/latest/scale-to-width-down/1200?cb=20190104124219')
+            .setImage('https://static.wikia.nocookie.net/villainsfanon/images/4/4e/Troll-Face-Meme-PNG.png/revision/latest/scale-to-width-down/1200?cb=20190104124219')
             .setTimestamp()
 
         await interaction.reply({ embeds: [aboutEmbed] })
