8405fb43e348a06600a8d189e202d90583de4497
Andreas Miller <73205713+A9qx@users.noreply.github.com>
2023-08-28 10:23:22 -0600

    added contributor field

--- commands/utility/version.js
+++ commands/utility/version.js
@@ -14,10 +14,11 @@ module.exports = {
                 { name: 'Build:', value: 'Stable', inline: true },
                 { name: 'Number:', value: '1.0.0', inline: true },
                 { name: 'Node Version:', value: process.version, inline: true },
+                { name: 'Contributors:', value: 'Falsonix', inline: true }
             )
             .setTimestamp()
             .setURL("https://github.com/A9qx/Christian")
-            .setFooter({ text: 'Created by A9qx with contributions from Falsonix', iconURL: 'https://cdn.discordapp.com/avatars/563087321704955914/76bae140071868346e001db57fd3c16a.webp' });
+            .setFooter({ text: 'Created by A9qx', iconURL: 'https://cdn.discordapp.com/avatars/563087321704955914/76bae140071868346e001db57fd3c16a.webp' });
 
         await interaction.reply({ embeds: [veresionEmbed] })
     },
