146f5aa9aa73d06a5289db3317d25c10df967f72
Andreas Miller <73205713+A9qx@users.noreply.github.com>
2023-08-28 16:08:54 -0600

    changed to v1.1.1

--- commands/utility/version.js
+++ commands/utility/version.js
@@ -8,10 +8,10 @@ module.exports = {
         const veresionEmbed = new EmbedBuilder()
             .setTitle("Version:")
             .setColor("#ffffff")
-            .setDescription("**What's Changed**:\n* added more quotes by @falsonix in https://github.com/A9qx/Christian/pull/1\n* added even MORE quotes lmao by @falsonix in https://github.com/A9qx/Christian/pull/2\n\n**New Contributors**\n* @falsonix made their first contribution in https://github.com/A9qx/Christian/pull/1\n**Full Changelog**: https://github.com/A9qx/Christian/compare/v1.0.0...v1.1.0")
+            .setDescription("**What's Changed**:\n* i added more quotes lol by @falsonix in https://github.com/A9qx/Christian/pull/3\n**Full Changelog**: https://github.com/A9qx/Christian/compare/v1.1.0...v1.1.1")
             .addFields(
                 { name: 'Build:', value: 'Stable', inline: true },
-                { name: 'Number:', value: 'v1.1.0', inline: true },
+                { name: 'Number:', value: 'v1.1.1', inline: true },
                 { name: 'Node Version:', value: process.version, inline: true },
                 { name: 'Contributors:', value: 'Falsonix', inline: true }
             )
