e6c37204ac87d1dd497c82cd58beee3c2cec33f2
A9qx <aviationismylife100@gmail.comls>
2024-03-30 21:17:42 -0600

    added glucose command info

--- commands/utility/about.js
+++ commands/utility/about.js
@@ -18,7 +18,6 @@ module.exports = {
             .setURL('https://github.com/A9qx/Christian')
             .setImage('https://static.wikia.nocookie.net/villainsfanon/images/4/4e/Troll-Face-Meme-PNG.png/revision/latest/scale-to-width-down/1200?cb=20190104124219')
             .addFields(
-                { name: 'Uptime:', value: uptimeString, inline: true },
                 { name: '/about', value: 'Get information about the bot.', inline: true },
                 { name: '/ping', value: 'Tests the latency between you and Discord!', inline: true },
                 { name: '/version', value: 'View information regarding this build.', inline: true },
@@ -27,6 +26,7 @@ module.exports = {
                 { name: '/joke', value: 'Get a random joke', inline: true },
                 { name: '/quote', value: "I will give you something I've actually before.", inline: true },
                 { name: '/rps', value: 'Play rock-paper-scissors with Me!', inline: true },
+                { name: '/glucose', value: 'Retrieve Blood Glucose data from Nightscout.', inline: true}
             )
             .setTimestamp()
             .setFooter({ text: 'Created by A9qx', iconURL: 'https://images-ext-1.discordapp.net/external/cVs7GOQJOQHkwVkB_d7Qis2E699lFvU9CEKn4QvjVWs/%3Fcb%3D20190104124219/https/static.wikia.nocookie.net/villainsfanon/images/4/4e/Troll-Face-Meme-PNG.png/revision/latest/scale-to-width-down/1200?format=webp&width=1742&height=1418' });
