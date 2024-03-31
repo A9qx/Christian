ba2415b1859be469de16331a80b44be60ea09fb8
A9qx <aviationismylife100@gmail.comls>
2024-03-30 21:13:42 -0600

    updated version info

--- commands/utility/version.js
+++ commands/utility/version.js
@@ -18,13 +18,13 @@ module.exports = {
             .addFields(
                 { name: 'Uptime:', value: uptimeString, inline: true },
                 { name: 'Build:', value: 'Stable', inline: true },
-                { name: 'Number:', value: 'v1.1.3', inline: true },
+                { name: 'Number:', value: 'v1.2.0', inline: true },
                 { name: 'Node Version:', value: process.version, inline: true },
                 { name: 'Contributors:', value: 'Falsonix', inline: true }
             )
             .setTimestamp()
             .setURL("https://github.com/A9qx/Christian")
-            .setFooter({ text: 'Created by A9qx', iconURL: 'https://cdn.discordapp.com/avatars/563087321704955914/76bae140071868346e001db57fd3c16a.webp' });
+            .setFooter({ text: 'Created by A9qx', iconURL: 'https://images-ext-1.discordapp.net/external/cVs7GOQJOQHkwVkB_d7Qis2E699lFvU9CEKn4QvjVWs/%3Fcb%3D20190104124219/https/static.wikia.nocookie.net/villainsfanon/images/4/4e/Troll-Face-Meme-PNG.png/revision/latest/scale-to-width-down/1200?format=webp&width=1742&height=1418' });
 
         await interaction.reply({ embeds: [versionEmbed] })
     },
